842c4e59f4c40ac1f52a4e6f367c4c1885760eb4
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-23 20:24:11 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
 class Customer
-  
+
+  attr_accessor :name, :age #customers can change name and age
+
+  @@all=[] #tracks all instances of customers created/......
+
+  def initialize(name, age)# each customer has name and age
+    @name=name#name and age are set at initialization
+    @age=age
+    @@all << self#.... /at initialization
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)# instead of setting customer, we set self, because we're passing in self as a reference to the current instance of customer
+  end
+
+  def meals
+    Meals.all.select do |meal|
+      meal.customer == self
+    end 
+  end
+
+
+
 end
