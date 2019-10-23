162cb31cbdeeeed2ba0a956803a7a7ed85e70a8d
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-22 20:47:03 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+    attr_accessor :meals, :waiter
+    attr_reader :name, :age
+    @@all=[]
+
+    def initialize(name, age)
+        @name=name
+        @age=age
+        @@all<< self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiters,total,tip)
+        Meal.new(waiters,self,total,tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.customer==self}
+    end
+
+    def waiters
+        meals.map{|meal| meal.waiter}
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
 class Waiter
+    attr_accessor :customer, :meal
+    attr_reader :name, :years_experience
+    @@all=[]
+
+    def initialize(name,years_experience)
+        @name=name
+        @years_experience=years_experience
+        @@all<< self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customers,total,tip)
+        Meal.new(self,customers,total,tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.waiter==self}
+    end
+
+    def best_tipper
+        meals.max{|a,b| a.tip<=>b.tip}.customer
+    end
+
+
 
 end
